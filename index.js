1dc8cc50d6e60c5ae7272c4f7d8b6d14857d4cd4
Romy <701224@student.inholland.nl>
2026-06-21 19:40:31 +0200

    add light/dark modus

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import { initGlassesAnimation } from "./helpers/animation.js";
 
-initGlassesAnimation();
+//https://dev.to/natclark/checking-for-reduced-motion-preference-in-javascript-4lp9
+const isReducedMotion = window.matchMedia(`(prefers-reduced-motion: reduce)`) === true || window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
+
+if (!isReducedMotion) {
+    initGlassesAnimation();
+}
 
